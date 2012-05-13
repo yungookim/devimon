fb534a3b92318b171003e265e0e548b34d7e1442
KimY <yungookim@gmail.com>
2012-05-13 04:15:48 +0000

    wrong brach. merging to socket

--- app.js
+++ app.js
@@ -8,11 +8,12 @@ var MESSAGE = ", you should go check your device right now!";
 
 //io.set('close timeout', 10);
 io.set('heartbeat timeout', 30);
+
 //io.set('log level', 3);
 app.listen(4000);
 
 app.get('/', function (req, res) {
-  res.sendfile(__dirname + '/index.html');
+  res.sendfile(__dirname + '/html/index.html');
 });
 
 io.sockets.on('connection', function (socket) {
@@ -33,20 +34,21 @@ io.sockets.on('connection', function (socket) {
 		if (!CLOSE_REQUESTED){
 			var phone = client.getPhoneNumber('+16479316110');
 			phone.setup(function(){
-				phone.sendSms(NUMBER, NAME + MESSAGE, null, function(result){
-					console.log(result);
+				//phone.sendSms(NUMBER, NAME + MESSAGE, null, function(result){
+					//console.log(result);
+					console.log("assume txt msg sent");
 					return;
 					// if (call.smsDetails.status != 'queued'){
 					//msging failed. try again after a few seconds?
 					// }	
-				});
+				//});
 			});
 		} else {
 			console.log('log it and current socket is gone');
+			return;
 		}
 	});
 	socket.on('client_close', function(){
-		console.log('=====disconnecting');
 		CLOSE_REQUESTED = true;
 		socket.disconnect();
 	});
