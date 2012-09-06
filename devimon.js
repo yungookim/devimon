d37f56960ebd180caaf417f07aa8742c780639c1
KimY <yungookim@gmail.com>
2012-09-06 00:16:51 -0400

    init

--- devimon.js
+++ devimon.js
@@ -1,26 +1,19 @@
-var app = require('express').createServer(),
-    io = require('socket.io').listen(app),
+var express = require('express');
+    io = require('socket.io'),
     TwilioClient = require('twilio').Client,
     client = new TwilioClient('AC724b4080ddd54f7b8f76c5635b7c13da',
 			      'bb72ce3adfed239513c4ac8ead423feb', 
                               '69.164.219.86');
-	
-var MESSAGE = ", you should go check your device right now!";
-var COUNTRY_NUMBER = {canada : '+16479316110'};
+var app = module.exports = express.createServer();
+io.listen(app);
 
-//these setters should be dynamic
-io.set('heartbeat timeout', 5);
-io.set('heartbeat interval', 5);
-io.set('log level', 4);
-
-app.listen(4000);
-
-app.get('/', function (req, res) {
-	res.sendfile(__dirname + '/html/index.html');
+app.configure(function(){
+ 	app.use(express.static(__dirname + '/public'));	
+ 	app.use(express.bodyParser());
 });
 
-app.get('/devimon_client.js', function (req, res) {
-	res.sendfile(__dirname + '/html/js/devimon_client.js');
+app.get('/', function (req, res) {
+	res.render('index');
 });
 
 app.get('/admin', function (req, res){
@@ -28,56 +21,68 @@ app.get('/admin', function (req, res){
 	res.send("Number Connected : " + io.sockets.clients().length);
 });
 
-//Handler for each socket connection
-io.sockets.on('connection', function (socket) {
-	var NUMBER, NAME, CLOSE_REQUESTED;
+app.listen(4000);
 
-	console.log(socket.namespace.manager.settings);
+//these setters should be dynamic
+// io.set('heartbeat timeout', 5);
+// io.set('heartbeat interval', 5);
+// io.set('log level', 4);
 
-	//Initialize, check id&pass
-	socket.on('clientInfo', function (data) {
-		console.log("Number : " + data.number);
-		if(data.pass != "wat!"){
-			socket.emit("passErr", null);
-			CLOSE_REQUESTED = true;
-			socket.disconnect();
-			return;
-		}
-		NUMBER = data.number;
-		NAME = data.name;
-		CLOSE_REQUESTED = false;
+var MESSAGE = ", you should go check your device right now!";
+var COUNTRY_NUMBER = {canada : '+16479316110'};
 
-		socket.emit('init', { socket_id : socket.id, status : "ok"});
-	});
+// //Handler for each socket connection
+// io.sockets.on('connection', function (socket) {
+// 	var NUMBER, NAME, CLOSE_REQUESTED;
 
-	//Disconnect handler
-	socket.on('disconnect', function () {
-		console.log("close requested : " + CLOSE_REQUESTED);
-		//Client illegal shut down
-		if (CLOSE_REQUESTED == false){
-			//This number has to change according to the Country
-			var phone = client.getPhoneNumber(COUNTRY_NUMBER['canada']);
-			phone.setup(function(){
-				phone.sendSms(NUMBER, NAME + MESSAGE, null, function(result){
-					if (result.smsDetails.status == 'queued'){
-						//success
-					} else {
-						//sms failed
-					}
-					return;
-				});
-			});
-		} else if (CLOSE_REQUESTED == true){
-			console.log('current socket is goner');
-			return;
-		} else {
-			console.log("client trying to reconnect after illegal shutdown");
-			return;
-		}
-	});
+// 	console.log(socket.namespace.manager.settings);
 
-	socket.on('client_close', function(){
-		CLOSE_REQUESTED = true;
-		socket.disconnect();
-	});
-});
+// 	//Initialize, check id&pass
+// 	socket.on('clientInfo', function (data) {
+// 		console.log("Number : " + data.number);
+// 		if(data.pass != "wat!"){
+// 			socket.emit("passErr", null);
+// 			CLOSE_REQUESTED = true;
+// 			socket.disconnect();
+// 			return;
+// 		}
+// 		NUMBER = data.number;
+// 		NAME = data.name;
+// 		CLOSE_REQUESTED = false;
+
+// 		socket.emit('init', { socket_id : socket.id, status : "ok"});
+// 	});
+
+// 	//Disconnect handler
+// 	socket.on('disconnect', function () {
+// 		console.log("close requested : " + CLOSE_REQUESTED);
+// 		//Client illegal shut down
+// 		if (CLOSE_REQUESTED == false){
+// 			//This number has to change according to the Country
+// 			var phone = client.getPhoneNumber(COUNTRY_NUMBER['canada']);
+// 			phone.setup(function(){
+// 				phone.sendSms(NUMBER, NAME + MESSAGE, null, function(result){
+// 					if (result.smsDetails.status == 'queued'){
+// 						//success
+// 					} else {
+// 						//sms failed
+// 					}
+// 					return;
+// 				});
+// 			});
+// 		} else if (CLOSE_REQUESTED == true){
+// 			console.log('current socket is goner');
+// 			return;
+// 		} else {
+// 			console.log("client trying to reconnect after illegal shutdown");
+// 			return;
+// 		}
+// 	});
+
+// 	socket.on('client_close', function(){
+// 		CLOSE_REQUESTED = true;
+// 		socket.disconnect();
+// 	});
+// });
+
+
